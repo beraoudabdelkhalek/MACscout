ee499dbb5eae8a9c3656388fbb9898e98613b6c2
Abdelkhalek Beraoud <89158254+beraoudabdelkhalek@users.noreply.github.com>
2023-06-14 18:35:45 +0100

    Update scout.py

--- scout.py
+++ scout.py
@@ -1,80 +1,87 @@
+#!/usr/bin/python3
 import sys
 import json
 import re
 import random
 
-
 #the database is from this website: https://maclookup.app/
-file = open("./mac-vendors.json", encoding="utf8")
-data = json.load(file)
+try:
+    file = open("./mac-vendors.json", encoding="utf8")
+    data = json.load(file)
 
-
-def generate_mac_address():
-    mac_address = [random.randint(0x00, 0xff) for _ in range(6)]
-    mac_address_str = ':'.join(['{:02X}'.format(byte) for byte in mac_address])
-    return mac_address_str
+    def generate_mac_address():
+        mac_address = [random.randint(0x00, 0xff) for _ in range(6)]
+        mac_address_str = ':'.join(['{:02X}'.format(byte) for byte in mac_address])
+        return mac_address_str
 
 #concatenate an existing OUI with a random individual address to get a valid mac address
-def generate_mac_address_valid():
-    mac_address = [random.randint(0x00, 0xff) for _ in range(3)]
-    mac_address_str = ':'.join(['{:02X}'.format(byte) for byte in mac_address])
-    random.shuffle(data)
-    for element in data:
-        if element["blockType"]=="MA-L": 
-            return element["macPrefix"]+":"+mac_address_str
-
-
-def is_valid_mac_address(mac_address):
-    regex = re.compile(r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$')
-    return regex.match(mac_address) is not None
-
-def is_valid_oui(oui):
-    regex = re.compile(r'^([0-9A-Fa-f]{2}[:-]){2}([0-9A-Fa-f]{2})$')
-    return regex.match(oui) is not None
-
-#get the vendor name from the max using a complete mac address or just the OUI
-def getVendor(mac):
-    macadr=mac.upper()
-    if is_valid_mac_address(macadr) or is_valid_oui(macadr):
+    def generate_mac_address_valid():
+        mac_address = [random.randint(0x00, 0xff) for _ in range(3)]
+        mac_address_str = ':'.join(['{:02X}'.format(byte) for byte in mac_address])
+        random.shuffle(data)
         for element in data:
-            if element["macPrefix"]==macadr[0:8]:
+            if element["blockType"]=="MA-L": 
+                return element["macPrefix"]+":"+mac_address_str
+
+
+    def is_valid_mac_address(mac_address):
+        regex = re.compile(r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$')
+        return regex.match(mac_address) is not None
+
+    def is_valid_oui(oui):
+        regex = re.compile(r'^([0-9A-Fa-f]{2}[:-]){2}([0-9A-Fa-f]{2})$')
+        return regex.match(oui) is not None
+
+    #get the vendor name from the max using a complete mac address or just the OUI
+    def getVendor(mac):
+        macadr=mac.upper()
+        if is_valid_mac_address(macadr) or is_valid_oui(macadr):
+            for element in data:
+                if element["macPrefix"]==macadr[0:8]:
+                    return element["vendorName"]
+            return "vendor not found"
+        else: return "invalid input"
+
+    #get the vendor name using MAC address blocks
+    def getVendorStrict(mac):
+        macadr=mac.upper()
+        for element in data:
+            if element["macPrefix"]==macadr:
                 return element["vendorName"]
-        return "vendor not found"
-    else: return "invalid input"
+        return "not found"
+    #get the MAC prefix of the vendor using the vendor's name
+    def getMACPrefixVendor(vendor):
+        for element in data:
+            if element["vendorName"].lower()==vendor.lower():
+                return element["macPrefix"]
+        return "not found"
 
-#get the vendor name using MAC address blocks
-def getVendorStrict(mac):
-    macadr=mac.upper()
-    for element in data:
-        if element["macPrefix"]==macadr:
-            return element["vendorName"]
-    return "not found"
+    listargs=sys.argv
 
-def getMACPrefixVendor(vendor):
-    for element in data:
-        if element["vendorName"].lower()==vendor.lower():
-            return element["macPrefix"]
-    return "not found"
+    if len(listargs)>1:
+        if listargs[1]=="-h":
+            print('''\n __  __    _    ____                     _   
+|  \/  |  / \  / ___|___  ___ ___  _   _| |_ 
+| |\/| | / _ \| |   / __|/ __/ _ \| | | | __|
+| |  | |/ ___ \ |___\__ \ (_| (_) | |_| | |_ 
+|_|  |_/_/   \_\____|___/\___\___/ \__,_|\__|\nby Abdelkhalek Beraoud(letmewin)\n\nuse the following:\n-h: show the list of commands\n-m [MAC address or OUI]: get the vendor name from the specified MAC adress or OUI\n-v [\"vendor name\"]: get the OUI from the vendor name\n-vs [MAC address block]: get the vendor name using any MAC address block type\n-g: generate a random MAC address\n-gv: get a mac address composed of an existing OUI and a random individual address''')
+        elif listargs[1]=="-m":
+            print(getVendor(listargs[2]))
+        elif listargs[1]=="-v":
+            print(getMACPrefixVendor(listargs[2]))
+        elif listargs[1]=="-vs":
+            print(getVendorStrict(listargs[2]))
+        elif listargs[1]=="-g":
+            print(generate_mac_address())
+        elif listargs[1]=="-gv":
+            print(generate_mac_address_valid())
+        else: print("invalid command")
+    else: print('''\n __  __    _    ____                     _   
+|  \/  |  / \  / ___|___  ___ ___  _   _| |_ 
+| |\/| | / _ \| |   / __|/ __/ _ \| | | | __|
+| |  | |/ ___ \ |___\__ \ (_| (_) | |_| | |_ 
+|_|  |_/_/   \_\____|___/\___\___/ \__,_|\__|\nby Abdelkhalek Beraoud(letmewin)\n\nuse -h for help''')
 
-listargs=sys.argv
-
-if len(listargs)>1:
-    if listargs[1]=="-h":
-        print("\nuse the following:\n-h: show list of parameters\n-m [MAC address or OUI]: get the vendor name from the specified MAC adress or OUI\n-v [\"vendor name\"]: get the OUI from the vendor name\n-vs [MAC address block]: get the vendor name using any MAC address block type\n-g: generate a random MAC address\n-gv: get a mac address composed of an existing OUI and a random individual address")
-    elif listargs[1]=="-m":
-        print(getVendor(listargs[2]))
-    elif listargs[1]=="-v":
-        print(getMACPrefixVendor(listargs[2]))
-        print(listargs[2])
-    elif listargs[1]=="-vs":
-        print(getVendorStrict(listargs[2]))
-    elif listargs[1]=="-g":
-        print(generate_mac_address())
-    elif listargs[1]=="-gv":
-        print(generate_mac_address_valid())
-else: print("\nMACscout by Abdelkhalek Beraoud(letmewin)\n\nuse -h for help")
-
-file.close()
-    
-
-
+    file.close()
+except:
+    print("an error has occured while reading the file")
